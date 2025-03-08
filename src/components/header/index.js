7842505bcc144e3798046649d6975650ff23f859
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-03-08 15:14:12 -0500

    imported Github logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -8,6 +8,11 @@ const Header = () => {
                 <div className='header-logo'>
                     Expensr <i class="fi fi-rr-credit-card"></i>
                 </div>
+                <div className="header-button">
+                    <a href="https://github.com/CoreyPhillipsOfficial" target='_blank' rel='noopener noreferrer'>
+                        <i class="devicon-github-original"></i> Star
+                    </a>
+                </div>
             </div>
         </div>
     )
