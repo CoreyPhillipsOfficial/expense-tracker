4ac6ecd0bcad934858092b277dde48b771c916ca
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-03-09 18:26:33 -0400

    imported ADD_EXPENSE in expenses.js

--- src/pages/add-expense/index.js
+++ src/pages/add-expense/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import TopFold from '../../components/topfold'
+import './add-expense.css'
 
 const AddExpense = () => {
     return (
-        <div>
+        <div className='add-expense'>
             <TopFold />
         </div>
     )
