5aeaacd3dfb81ba4f3df4052ba51b6d95ac7c483
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-03-09 12:53:41 -0400

    added routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Home from "./pages/home";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './components/header/index.js'
+import Home from "./pages/home";
+import AddExpense from './pages/add-expense/index.js';
 
 function App() {
-  return <div>
-    <Header />
-    <Home />
-    <div>Footer</div>
-  </div>
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/add-expense' component={AddExpense} />
+        </Routes>
+        <Home />
+        <div>Footer</div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/add-expense/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddExpense = () => {
+    return (
+        <div>This is add expense</div>
+    )
+}
+
+export default AddExpense
