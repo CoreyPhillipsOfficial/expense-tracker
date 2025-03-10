8485f68de10ec23fa4710b630b5823b46191aba1
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2025-03-10 19:07:32 -0400

    modal if functional

--- src/components/add-form/success-modal.js
+++ src/components/add-form/success-modal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './success-modal.css'
 import Modal from 'react-modal';
+import { Link } from 'react-router-dom';
 
 const SuccessModal = () => {
     const customStyles = {
@@ -11,10 +12,29 @@ const SuccessModal = () => {
             bottom: 'auto',
             marginRight: '-50%',
             transform: 'translate(-50%, -50%)',
+            backgroundColor: '#71ccca',
+            borderRadius: '12px'
         },
     };
     return (
-        <Modal isOpen={true} style={customStyles}></Modal>
+        <>
+            <Modal isOpen={true} style={customStyles}>
+                <div className="modal-inner">
+                    <label>Expense added successfully!</label>
+                    <img
+                        src={require('../../assets/images/added-image.png')}
+                        alt='Expense Added'
+                        className='added-image'
+                    />
+                    <Link to='/'>
+                        <div className='take-home-button'>
+                            <i className='fi-rr-home'></i>
+                            Home
+                        </div>
+                    </Link>
+                </div>
+            </Modal>
+        </>
     )
 }
 
